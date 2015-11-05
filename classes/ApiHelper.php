131a1a12a8941e0ba703bc33066ab87174c02f1b
Alex Lushpai <gwinn@users.noreply.github.com>
2015-11-05 16:43:26 +0300

    Update ApiHelper.php

--- classes/ApiHelper.php
+++ classes/ApiHelper.php
@@ -24,26 +24,30 @@ class ApiHelper {
     public function processXMLOrders() {
         $url = $this->config['tiu_xml_url'];
         $xml = simplexml_load_file($url);
+
         if (! $xml instanceof SimpleXMLElement) {
             $this->writeLog("ApiHelper::processXmlOrders: cannot get XML from $url");
             return false;
         }
 
         $orders = array();
+
         foreach($xml as $xmlOrder) {
             $order = $this->parser->parseXMLNewOrder($xmlOrder);
+
             if ($order) {
                 $customerId = $this->checkCustomers($order);
-                if ($customerId === false) {
-                    echo "upload failed" . PHP_EOL;
-                    return false;
-                } elseif ($customerId !== 0) {
+
+                if ($customerId !== false) {
                     $order['customerId'] = $customerId;
                 }
+
                 $orders[$order['number']] = $order;
             }
         }
+
         $orders = $this->filterOrders($orders);
+
         if ($this->uploadOrders($orders)) {
             $a = sizeof($orders);
             echo "uploaded $a orders" . PHP_EOL;
@@ -81,11 +85,10 @@ class ApiHelper {
     }
 
     protected function checkCustomers($order) {
-        $customerId = 0;
-        if ($order['email'] != '')
-            $filter = array('email' => $order['email']);
-        if ($order['phone'] != '')
-            $filter = array('name' => $order['phone']);
+        $customerId = false;
+        if ($order['email'] != '') $filter['email']= $order['email'];
+        if ($order['phone'] != '') $filter['name'] = $order['phone'];
+
         if (isset($filter)) {
             try {
                 $customers = $this->crmClient->customersList($filter);
@@ -96,17 +99,23 @@ class ApiHelper {
                 );
                 return false;
             }
-            if (isset($customers['customers']) && sizeof($customers['customers'] && isset($customers['customers'][0]['externalId']))) {
-                $customerId = $customers['customers'][0]['externalId'];
+
+            if (!empty($customers['customers'])) {
+                foreach ($customers as $_customer) {
+                    if (!empty($_customer['externalId'])) {
+                        $customerId = $_customer['externalId'];
+                        break;
+                    }
+                }
             }
-            time_nanosleep(0, 200000000);
+
         }
+
         return $customerId;
     }
 
     protected function writeLog($text, $type = null) {
-        if (! file_exists(__DIR__ ."/../logs"))
-            mkdir(__DIR__ ."/../logs");
+        if (! file_exists(__DIR__ ."/../logs")) mkdir(__DIR__ ."/../logs");
         $date = date('Y-m-d H:i:s');
         file_put_contents(__DIR__ . "/../logs/error.log", "[$date]$text" . PHP_EOL, FILE_APPEND);
         if ($type == 'error') {
@@ -120,34 +129,13 @@ class ApiHelper {
 
     protected function filterOrders($toUpload) {
         $numbers = array_keys($toUpload);
-        if (date_create_from_format('Y-m-d H:i:s', $this->config['filter_date'])) {
+        if (date_create_from_format('Y-m-d H:i:s', $this->config['date_from'])) {
             foreach ($toUpload as $i => $order) {
-                if ($order['createdAt'] < $this->config['filter_date']) {
+                if ($order['createdAt'] < $this->config['date_from']) {
                     unset($toUpload[$i]);
                 }
             }
         }
-
-        $ordersListPage = 0;
-        do {
-            $ordersListPage++;
-            try {
-                $orders = $this->crmClient->ordersList(array(
-                    'numbers' => $numbers
-                ), $ordersListPage, 100);
-            } catch (\RetailCrm\Exception\CurlException $e) {
-                $text = '\Retailcrm\ApiClient::ordersList: ' . $e;
-                $this->writeLog($text, 'error');
-                return false;
-            }
-            if (isset($orders['orders']) && sizeof($orders['orders'])) {
-                foreach ($orders['orders'] as $order) {
-                    if (isset($toUpload[$order['number']]))
-                        unset($toUpload[$order['number']]);
-                }
-            }
-            time_nanosleep(0, 200000000);
-        } while ($ordersListPage < $orders['pagination']['totalPageCount']);
 
         return $toUpload;
     }
