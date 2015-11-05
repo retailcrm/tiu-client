ec353829f305f4377a0fd4c5b30a455f9732eddf
Alex Lushpai <gwinn@users.noreply.github.com>
2015-11-05 16:43:03 +0300

    Update run.php

--- run.php
+++ run.php
@@ -4,17 +4,24 @@ require(__DIR__ . "/config/config.php");
 
 if (! file_exists(__DIR__ . "/logs"))
     mkdir(__DIR__ . "/logs");
+
 if (file_exists(__DIR__ . "/logs/cookie.txt"))
     unlink(__DIR__ . "/logs/cookie.txt");
 
 $lockFile = __DIR__ . "/run.lock";
+
 if (file_exists($lockFile)) {
     if ((int)file_get_contents($lockFile) > time()) {
         echo "script is busy";
         exit();
     }
 }
+
 file_put_contents("run.lock", strtotime('+5 minutes'));
+
+if (file_exists(__DIR__ . "/logs/sync.log")) {
+    $config['date_from'] = file_get_contents(__DIR__ . "/logs/sync.log");
+}
 
 $apiHelper = new ApiHelper($сonfig);
 
